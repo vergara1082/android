18d2a793e395ad61b98a5fec5c18c944846a1f62
vergara1082 <jjvergara79@misena.edu.co>
2018-12-28 22:03:58 -0500

    la aplicacion esta casi funcionando

--- src/main.py
+++ src/main.py
@@ -1,6 +1,17 @@
- from flask import Flask
+from flask import Flask
 app = Flask(__name__)
+
+
+def suma(a,b):
+  return a+b
+
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+  res = suma(10,20)
+  return "Hello World! %s" % (res)
+  
+  
+ 
+if __name__ == "__main__": 
+  app.run(host='0.0.0.0', port=5000)
---
+++ src/test_main.py~
@@ -0,0 +1,4 @@
+from main import *
+
+def test_suma():
+  assert suma(10,20) == 30
